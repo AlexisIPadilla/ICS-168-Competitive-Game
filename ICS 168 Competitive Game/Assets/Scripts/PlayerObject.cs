38c906c32cd2193f3f4214ac26e40b4d62a6590e
Alexis Padilla <aipadill@uci.edu>
2019-05-25 14:47:39 -0700

    Start of Networking
    Networking is still a work in progress.

---
+++ ICS 168 Competitive Game/Assets/Scripts/PlayerObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerObject : NetworkBehaviour
+{
+    public GameObject playerUnitPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isLocalPlayer == false)
+        {
+            return;
+        }
+        Debug.Log("PlayerObject::Start -- Spawning my own personal unit");
+        Instantiate(playerUnitPrefab);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
